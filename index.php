e518f6334559201f2e2b7ae77851015800c83f69
Jae Basagre <basagrejae@gmail.com>
2025-06-03 10:03:44 +0800

    index page and database

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+</head>
+
+<body>
+    
+    <h3>Login</h3>
+
+    <form action="includes/login.inc.php" method="POST">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <button>Login</button>
+    </form>
+
+    <h3>Signup</h3>
+
+    <form action="includes/signup.inc.php" method="POST">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="E-Mail">
+        <button>Signup</button>
+    </form>
+
+</body>
+
+</html>
