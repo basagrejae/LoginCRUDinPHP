071746cf309f0494aca3114efad6caf082468902
Jae Basagre <basagrejae@gmail.com>
2025-06-03 10:46:26 +0800

    set cookie parameters

---
+++ includes/config_session.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+ini_set('session.use_only_cookies', 1); // set to true
+ini_set('session.use_strict_mode', 1);
+
+session_set_cookie_params([
+    'lifetime' => 1800,
+    'domain' => 'localhost',
+    'path' => '/',
+    'secure' => true,
+    'httponly' => true
+]);
