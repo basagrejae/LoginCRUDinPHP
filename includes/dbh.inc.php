08a7cb1eec448d06fab1cea42307e5006519db1e
Jae Basagre <basagrejae@gmail.com>
2025-06-03 10:38:33 +0800

    Connection string to SQL

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,3 +4,13 @@ $host = 'localhost';
 $dbname = 'myfirstdatabase'; // name of database in sql
 $dbusername = 'root';
 $dbpassword = '';
+
+try {
+    //code...
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", dbusername,
+    dbpassword); // connection string to SQL
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // throw exception
+    die("Connection Failed: " . $e->getMessage()); // terminates the script if something goes wrong
+}
