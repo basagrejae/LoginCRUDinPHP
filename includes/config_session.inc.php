acfd685b91484b9ad7e33511f326b725e61b3282
Jae Basagre <basagrejae@gmail.com>
2025-06-03 10:59:20 +0800

    create a session

--- includes/config_session.inc.php
+++ includes/config_session.inc.php
@@ -10,3 +10,16 @@ session_set_cookie_params([
     'secure' => true,
     'httponly' => true
 ]);
+
+session_start(); 
+
+if (!isset($_SESSION["last_generation"])) { // isset function checks if session exists inside webpage
+    session_regenerate_id();
+    $_SESSION["last_generation"] = time();
+} else {
+    $interval = 60 * 30; // set time to 30 mins
+    if (time() - $_SESSION["last_generation"] >= $interval) { // time minus last_generation greater than equal interval
+        session_regenerate_id(); // renew session ID
+        $_SESSION["last_generation"] = time();
+    }
+}
