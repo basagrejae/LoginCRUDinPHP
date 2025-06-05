e4d8a428882c0f1155cf6e0c1def9df74474712b
Jae Basagre <basagrejae@gmail.com>
2025-06-05 20:27:08 +0800

    login page

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,7 +1,16 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD" === "POST"]) {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
     
     $username = $_POST["username"];
     $pwd = $_POST["pwd"];
+
+    try {
+    
+    } catch (PDOException $e) {
+        die("Query Failed: " . $e->getMessage());
+    }
+} else {
+    header("Location: ../index.php");
+    die();
 }
